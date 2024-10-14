06272b42eecc0eb7b65b985da0c27cbbf75ddce5
cribiking <Cribiking@gmail.com>
2024-10-14 17:56:04 +0200

    Quart canvi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import com.sun.security.jgss.GSSUtil;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Pene polla");
     }
 }
