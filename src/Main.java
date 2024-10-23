2612c67d49a42c557a71482bd8334b0f1a552d4e
cribiking <Cribiking@gmail.com>
2024-10-23 14:23:21 +0200

    Tokkenizer ya casi esta 2

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,36 @@ public class Main {
     public static void main(String[] args) {
         //Crearem una serie de proves per així poder guiar-nos
         //1. creació de arrays
-        //testCase_1();
-        testCase_2();
+        testCase_1();//true
+        testCase_2();//false
     }
 
     //REFERENCIA: https://stackoverflow.com/questions/322715/when-to-use-linkedlist-over-arraylist-in-java#:~:text=LinkedList%20is%20fast%20for%20appending,to%20delete%20in%20the%20middle.
     public static <E extends Symbol> boolean comprovarExpressio(List<E> list) { //List és genèric
 
+        Pila pila = new Pila();
         LinkedList<E> stack = new LinkedList<E>(); //Linked perque O(1) when index = 0 or index = list.size() - 1
 
         //TODO: Fer que et digui quin element falta
         for (E symbol : list) {
 
             if (symbol instanceof OpenClaudator || symbol instanceof OpenParentesi) {
-                stack.push(symbol);
+                //stack.push(symbol);
+                pila.push(symbol);
             } else if (symbol instanceof ClosingClaudator || symbol instanceof ClosingParentesi) {
-                if (stack.isEmpty()) {
+                if (pila.isEmpty()) {
                     return false;
                     /*
                      No hay un símbolo abierto correspondiente
                      Si el primer elemento es cerrado, retorna false directe
                     */
                 }
+                //TODO falta aquest metode a la pila
                 stack.pop(); // Se encuentra un símbolo cerrado, eliminar el abierto
             }
         }
 
-        return stack.isEmpty(); // Devuelve true si todos los símbolos están balanceados
+        return pila.isEmpty(); // Devuelve true si todos los símbolos están balanceados
     }
 
     public static void testCase_1() {
--- src/Pila.java
+++ src/Pila.java
@@ -1,21 +1,30 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.PrimitiveIterator;
+
 public class Pila implements InterfaceStack {
+    private final LinkedList<Object> stack = new LinkedList<Object>();
+    private final ListIterator<Object> l_it = stack.listIterator();
     @Override
     public void push(Object element) {
-
+        stack.add(element);
     }
-
     @Override
     public Object pop() {
         return null;
     }
 
     @Override
-    public int gesSize() {
+    public int getSize() {
         return 0;
     }
 
     @Override
     public boolean isEmpty() {
+        //stack.isEmpty();
+        while (l_it.hasNext()){
+            return true;
+        }
         return false;
     }
 }
