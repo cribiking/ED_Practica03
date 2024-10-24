1ed27988c11450353e36fa48b16d664fc120acc9
cribiking <Cribiking@gmail.com>
2024-10-24 23:37:45 +0200

    Acabado mi parte y he creado un test para el tuyo, si quieres al acabar lo implementamos sino no

---
+++ src/simuladorDeCues.java
@@ -0,0 +1,13 @@
+public class simuladorDeCues {
+
+    //public void simuladorDeCues() {
+//
+//        ListCua<Client> cuaCine = new ListCua<>();
+//        ArrayList caixers = new ArrayList();
+//
+//        for(int i = 0; i < NUM_CLIENTS; i++){
+//            cuaCine.inserir(new Client());
+//        }
+//
+//    }
+}
